e506419b10c507531e754475716884bcf2358fd9
Ashish Kumar <ashishks1911@gmail.com>
2024-08-28 00:20:10 +0530

    single element in a sorted array

--- src/org/ashish/binarysearch/_4_SingleElementInASortedArray.java
+++ src/org/ashish/binarysearch/_4_SingleElementInASortedArray.java
@@ -2,33 +2,47 @@ package org.ashish.binarysearch;
 
 public class _4_SingleElementInASortedArray {
     public static void main(String[] args) {
-        int []arr = {1,1,2,3,3,4,4,8,8};
+        int[] arr = {1, 1, 2, 3, 3, 4, 4, 8, 8};
         System.out.println(singleElement(arr));
+        System.out.println(optimalSolution(arr));
     }
 
-    public static int singleElement(int arr[]){
-        int lo = 0, hi = arr.length-1;
-        while (lo<=hi){
-            int mid  = lo + (hi-lo)/2;
-            if (lo==hi){
+    public static int singleElement(int arr[]) {
+        int lo = 0, hi = arr.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (lo == hi) {
                 return arr[lo];
             }
-            if (mid%2==0){
+            if (mid % 2 == 0) {
                 //even odd => single element is on right side
-                if (arr[mid]==arr[mid+1])
-                    lo=mid+1;
+                if (arr[mid] == arr[mid + 1])
+                    lo = mid + 1;
                 else
-                    hi=mid;
-            }
-            else {
+                    hi = mid;
+            } else {
                 //odd even => single element is on left side
-                if (arr[mid]==arr[mid-1]){
-                    lo = mid+1;
-                }
-                else
-                    hi=mid;
+                if (arr[mid] == arr[mid - 1]) {
+                    lo = mid + 1;
+                } else
+                    hi = mid;
             }
         }
         return -1;
+    }
+
+    public static int optimalSolution(int[] arr) {
+        int l = 0;
+        int r = arr.length - 1;
+
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (arr[mid] == arr[(mid ^ 1)]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return arr[l];
     }
 }
