f6659e6a50da93762f0dff24283e0f2dcccf67b9
Ashish Kumar <ashishks1911@gmail.com>
2024-08-30 12:57:07 +0530

    murthal parantha binary search solution

--- src/org/codingblocks/assignment/assignment4/MurthalParantha.java
+++ src/org/codingblocks/assignment/assignment4/MurthalParantha.java
@@ -12,20 +12,59 @@ public class MurthalParantha {
         for (int i = 0; i < l; i++) {
             rank[i] = sc.nextInt();
         }
-        System.out.println(timeNeeded(rank,p));
+        System.out.println(binarySearch(rank, p));
+    }
+
+    public static long binarySearch(int[] rank, int parantha) {
+        Arrays.sort(rank);
+        int lo = 0;
+        int ans = 0;
+        int hi = Integer.MAX_VALUE;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (isItPossible(rank, parantha, mid)) {
+                ans = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * time taken by cook with R rank
+     * R + 2R + 3R + 4R + .......nR    =>  R * (1+2+3+4+....n)
+     * =>  R * (n * (n-1)/2)
+     * given time >= R * (n*(n-1)/2)
+     * (2*given time)/ R >= n*(n-1)
+     */
+    public static boolean isItPossible(int[] rank, int parantha, int minTime) {
+        for (int i = 0; i < rank.length; i++) {
+            int t = 0;
+            int ct = 1;     // no. of parantha
+            while (t + ct * rank[i] <= minTime && parantha > 0) {
+                t += ct * rank[i];
+                ct++;
+                parantha--;
+            }
+            if (parantha == 0)
+                return true;
+        }
+        return false;
     }
 
     public static int timeNeeded(int[] rank, int parantha) {
         int[] timeArr = rank.clone();
         int prefix[] = rank.clone();
-        while (parantha!=0) {
+        while (parantha != 0) {
             System.out.println(Arrays.toString(prefix));
             int mini = minimumTime(prefix);
             for (int j = 0; j < rank.length; j++) {
                 if (mini == prefix[j]) {
                     parantha--;
                     timeArr[j] += rank[j];
-                    prefix[j]+=timeArr[j];
+                    prefix[j] += timeArr[j];
                     break;
                 }
             }
@@ -33,13 +72,13 @@ public class MurthalParantha {
         System.out.println(Arrays.toString(timeArr));
         System.out.println(Arrays.toString(prefix));
 
-        for (int i=0; i<rank.length; i++){
+        for (int i = 0; i < rank.length; i++) {
             prefix[i] -= timeArr[i];
         }
         System.out.println(Arrays.toString(prefix));
         int max = Integer.MIN_VALUE;
-        for (int i=0; i<prefix.length; i++){
-            if (prefix[i]>max){
+        for (int i = 0; i < prefix.length; i++) {
+            if (prefix[i] > max) {
                 max = prefix[i];
             }
         }
