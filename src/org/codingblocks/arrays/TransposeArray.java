57d2a967120ecc0fdc1a5a996b16c66815285711
Ashish Kumar <ashishks1911@gmail.com>
2024-09-26 11:17:03 +0530

    transpose of matrix

--- src/org/codingblocks/arrays/TransposeArray.java
+++ src/org/codingblocks/arrays/TransposeArray.java
@@ -1,24 +1,22 @@
 package org.codingblocks.arrays;
 
 /**
- *
  * Logic :
  * [0][0] [0][1] [0][2] [0][3]
  * [1][0] [1][1] [1][2] [1][3]
  * [2][0] [2][1] [2][2] [2][3]
  * [3][0] [3][1] [3][2] [3][3]
- *
+ * <p>
  * swap => [1][0] [0][1]
  * swap => [2][0] [0][2]
  * swap => [2][1] [1][2]
  * swap => [3][0] [0][3]
  * swap => [3][1] [1][3]
  * swap => [3][2] [2][3]
- *
  */
 public class TransposeArray {
     public static void main(String[] args) {
-        int [][] arr = {
+        int[][] arr = {
                 {2, 3, 1, 4},
                 {5, 6, 7, 9},
                 {8, 12, 11, 10},
@@ -30,15 +28,27 @@ public class TransposeArray {
 
     }
 
+    //only for square matrix
     private static void transpose(int[][] arr) {
-        for (int i=0; i<arr.length; i++){
-            for (int j=i+1; j<arr[i].length; j++){
-                if (i!=j) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr[i].length; j++) {
+                if (i != j) {
                     int temp = arr[i][j];
-                    arr[i][j] =  arr[j][i];
-                    arr[j][i]=temp;
+                    arr[i][j] = arr[j][i];
+                    arr[j][i] = temp;
                 }
             }
         }
+    }
+
+    //for all matrices
+    public int[][] transpose2(int[][] matrix) {
+        int ans[][] = new int[matrix[0].length][matrix.length];
+        for(int i=0; i<matrix[0].length; i++){
+            for(int j=0; j<matrix.length; j++){
+                ans[i][j] = matrix[j][i];
+            }
+        }
+        return ans;
     }
 }
