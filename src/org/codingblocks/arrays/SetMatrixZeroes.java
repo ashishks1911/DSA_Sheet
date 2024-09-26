75c86cb0ac425ab35b0a40a85164fc113c255750
Ashish Kumar <ashishks1911@gmail.com>
2024-09-26 19:10:38 +0530

    added set matrix zeroes

---
+++ src/org/codingblocks/arrays/SetMatrixZeroes.java
@@ -0,0 +1,85 @@
+package org.codingblocks.arrays;
+
+
+public class SetMatrixZeroes {
+    public static void main(String[] args) {
+        int arr[][] = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        int arr2[][] = {{-1}, {2}, {3}};
+        bruteForce(arr2);
+        display(arr2);
+
+    }
+
+    public static void bruteForce(int[][] matrix) {
+        int row[] = new int[matrix.length];
+        int col[] = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i] == 1 || col[j] == 1)
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+
+
+    public static void display(int arr[][]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void optimalSolution(int[][] matrix) {
+        // matrix[...][0] => used to mark row
+        // matrix[0][...] => used to mark column
+        int col0 = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    //check for col and row
+                    if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                        matrix[i][j] = 0;
+
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+}
+
+
+
