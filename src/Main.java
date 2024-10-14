271b73f59e5b0a2aa7854cbc0fdf9f65f7364734
Ashish Kumar <ashishks1911@gmail.com>
2024-10-14 22:02:58 +0530

    added permutations of an array

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int arr[] = {1,2,3};
-        List<List<Integer>> list =  permute(arr);
-        for (List<Integer> li : list){
-            System.out.println(li);
-        }
+
 //        subsequence(arr, 0, new ArrayList<>(), 3);
     }
     public static void subsequence(int []arr, int idx, List<Integer> li, int target){
@@ -36,27 +33,5 @@ public class Main {
             }
 
         }
-    }
-    private static void permutations(int [] nums, List<Integer> ds, List<List<Integer>> ans, boolean []freq){
-        if(ds.size() == nums.length){
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
-        for(int i = 0; i<nums.length; i++){
-            if(!freq[i]){
-                freq[i] = true;
-                ds.add(nums[i]);
-                permutations(nums, ds, ans, freq);
-                ds.remove(ds.size() - 1);
-                freq[i] = false;
-            }
-        }
-    }
-    public static List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean freq[] = new boolean[nums.length];
-        permutations(nums, ds, ans, freq);
-        return ans;
     }
 }
