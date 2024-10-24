f05139fd90c5eaa16ff250ffe6b8688f3b152501
Ashish Kumar <ashishks1911@gmail.com>
2024-10-24 20:12:32 +0530

    sudoku solver

---
+++ src/org/codingblocks/recursion/SudokuSolver2.java
@@ -0,0 +1,63 @@
+package org.codingblocks.recursion;
+
+public class SudokuSolver2 {
+    public static void main(String[] args) {
+        int[][] grid = {{3, 0, 6, 5, 0, 8, 4, 0, 0}, {5, 2, 0, 0, 0, 0, 0, 0, 0}, {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0}, {9, 0, 0, 8, 6, 3, 0, 0, 5}, {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0}, {0, 0, 0, 0, 0, 0, 0, 7, 4}, {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+        sudokuSolver(grid);
+        display(grid);
+    }
+
+    public static boolean sudokuSolver(int[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] == 0) {
+                    for (int i = 1; i <= 9; i++) {
+                        if (isSafe(board, row, col, i)) {
+                            board[row][col] = i;
+                            if (sudokuSolver(board))
+                                return true;
+                            else
+                                board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSafe(int[][] board, int row, int col, int val) {
+        for (int c = 0; c < board.length; c++) {
+            if (board[row][c] == val)
+                return false;
+        }
+        for (int r = 0; r < board.length; r++) {
+            if (board[r][col] == val)
+                return false;
+        }
+
+        int r = row - row % 3;
+        int c = col - col % 3;
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                if (board[i][j] == val)
+                    return false;
+            }
+        }
+        return true;
+
+    }
+
+
+    public static void display(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
