0587e65cef7a23f471ca9d4adbbe63224079ebbe
Ashish Kumar <ashishks1911@gmail.com>
2024-11-17 11:48:09 +0530

    price wise sorting

---
+++ src/org/codingblocks/lec35/Car.java
@@ -0,0 +1,27 @@
+package org.codingblocks.lec35;
+
+public class Car implements Comparable<Car>{
+    int price;
+    int speed;
+    String color;
+
+    public Car(int price, int speed, String color) {
+        this.price = price;
+        this.speed = speed;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "price=" + price +
+                ", speed=" + speed +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Car o) {
+        return this.price - o.price;
+    }
+}
---
+++ src/org/codingblocks/lec35/CarClient.java
@@ -0,0 +1,38 @@
+package org.codingblocks.lec35;
+
+public class CarClient {
+    public static void main(String[] args) {
+        Car[] arr = new Car[5];
+        arr[0] = new Car(200, 10, "white");
+        arr[1] = new Car(1000, 20, "black");
+        arr[2] = new Car(345, 3, "yellow");
+        arr[3] = new Car(34, 89, "Grey");
+        arr[4] = new Car(8907, 6, "red");
+        display(arr);
+        System.out.println("-------------------------------");
+        bubbleSort(arr);
+        display(arr);
+    }
+
+
+    public static <T> void display(T[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+    }
+
+    public static <T extends Comparable<T>> /*set bound*/ void bubbleSort(T[] arr) {
+        for (int turn = 1; turn < arr.length; turn++) {
+            for (int i = 0; i < arr.length - turn; i++) {
+                //if (arr[i] > arr[i + 1]) {      // comparison of addresses gives error
+                if (arr[i].compareTo(arr[i + 1])>0) {
+                    T temp = arr[i];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+}
