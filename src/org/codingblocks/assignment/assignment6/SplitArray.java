7df29f6307cb3fa9d40467dc5a5e6375ab112c34
Ashish Kumar <ashishks1911@gmail.com>
2024-11-29 10:59:15 +0530

    Split Array (Recursion) coding blocks

--- src/org/codingblocks/assignment/assignment6/SplitArray.java
+++ src/org/codingblocks/assignment/assignment6/SplitArray.java
@@ -5,22 +5,48 @@ import java.util.*;
 public class SplitArray {
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 3, 4, 5};
-        Set<List<Integer>> ans = new HashSet<>();
-        subset(arr,new ArrayList<Integer>(),ans,0);
-        for (List<Integer> li : ans){
-            if(li.stream().mapToInt(Integer::intValue).sum()==9)
-                System.out.println(li);
+        int sum = Arrays.stream(arr).sum();
+        if (sum%2==0) {
+            printSplits(arr, new ArrayList<>(), new ArrayList<>(), 0,0,0);
+            int count = countEqualSplits(arr, 0, 0, 0);
+            System.out.println(count);
+        }
+        else {
+            System.out.println(0);
         }
 
     }
-    public static void subset(int []arr, List<Integer> li,Set<List<Integer>> ans, int idx){
-        if (idx == arr.length){
-            ans.add(new ArrayList<>(li));
+
+    public static int countEqualSplits(int []arr, int sum1, int sum2, int idx){
+        if (idx==arr.length){
+            return sum1==sum2 ? 1 : 0;
+        }
+        int group1Count = countEqualSplits(arr, sum1+arr[idx], sum2, idx + 1);
+        int group2Count = countEqualSplits(arr, sum1, sum2+arr[idx], idx + 1);
+        return group1Count + group2Count;
+    }
+
+    public static void printSplits(int []arr, List<Integer> group1, List<Integer> group2, int sum1, int sum2, int idx){
+        if (idx==arr.length){
+            if (sum1==sum2){
+                for (int e : group1){
+                    System.out.print(e+" ");
+                }
+                System.out.print("and ");
+                for (int e: group2){
+                    System.out.print(e+" ");
+                }
+                System.out.println();
+            }
             return;
         }
-        li.add(arr[idx]);
-        subset(arr, li, ans, idx + 1);
-        li.remove(li.size() - 1);
-        subset(arr,li, ans, idx + 1);
+        group1.add(arr[idx]);
+        printSplits(arr, group1, group2, sum1+arr[idx], sum2, idx + 1);
+        group1.remove(group1.size() - 1);
+
+        group2.add(arr[idx]);
+        printSplits(arr, group1, group2, sum1, sum2+arr[idx], idx + 1);
+        group2.remove(group2.size() - 1);
+
     }
 }
