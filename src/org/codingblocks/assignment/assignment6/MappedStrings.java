c8858c84da7609a555cac09055543361e4e34334
Ashish Kumar <ashishks1911@gmail.com>
2024-11-29 17:58:03 +0530

    Solved Mapped Strings Problem Coding blocks

--- src/org/codingblocks/assignment/assignment6/MappedStrings.java
+++ src/org/codingblocks/assignment/assignment6/MappedStrings.java
@@ -1,33 +1,32 @@
 package org.codingblocks.assignment.assignment6;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class MappedStrings {
+
+    public static HashMap<Integer,Character> map = new HashMap<>();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String str = String.valueOf(n);
-        print(str, "", new ArrayList<>(), str);
+        int n  = sc.nextInt();
+        // HashMap<Integer,Character> map = new HashMap<>();
+        for(int i=1, ch='A'; i<=26; i++, ch++){
+            map.put(i, (char)ch);
+        }
+        print(String.valueOf(n), "");
     }
 
-    public static void print(String s, String ans, List<String> li, String org) {
-        if (s.length() == 0) {
-            for (String str : li) {
-                int num = Integer.parseInt(str);
-                System.out.print((char) (num - 1 + 'A'));
-            }
-            System.out.println();
+    public static void print(String s, String ans){
+        if(s.length()==0){
+            System.out.println(ans);
             return;
         }
-        for (int i = 1; i <= s.length(); i++) {
-            String st = s.substring(0, i);
-            if (!st.equals(org)) {
-                li.add(st);
-                print(s.substring(i), ans + st, li, org);
-                li.remove(li.size() - 1);
-            }
+        print(s.substring(1), ans+ map.get(Integer.parseInt(s.charAt(0)+"")));
+        if (s.length()>=2) {
+            String ch = s.substring(0, 2);
+            if (Integer.parseInt(ch) <= 26)
+                print(s.substring(2), ans + map.get(Integer.parseInt(ch)));
         }
     }
+
 }
