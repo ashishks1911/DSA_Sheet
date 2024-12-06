8f8421b96e2d3233f07af9e15f1e59d4b2d21bf2
Ashish Kumar <ashishks1911@gmail.com>
2024-12-06 11:08:09 +0530

    Added leetcode count primes

--- src/org/codingblocks/recursion/lec20/SeiveOfEratosthenes.java
+++ src/org/codingblocks/recursion/lec20/SeiveOfEratosthenes.java
@@ -2,16 +2,22 @@ package org.codingblocks.recursion.lec20;
 
 /*
     count prime number algo
+    problem : https://leetcode.com/problems/count-primes/description/
  */
 public class SeiveOfEratosthenes {
     public static void main(String[] args) {
-
+        int n = 100;
+        System.out.println(primeSieve(n));
     }
 
+    /*
+        0 -> marked prime
+        1 -> marked not prime
+     */
     public static int primeSieve(int n) {
         int[] prime = new int[n + 1];
-        prime[0] = 1;
-        prime[1] = 1;
+        prime[0] = 1;   //marked not prime
+        prime[1] = 1;   //marked not prime
         for (int i = 2; i * i <= prime.length; i++) {
             if (prime[i] == 0) {
                 for (int j = 2; i * j < prime.length; j++) {
