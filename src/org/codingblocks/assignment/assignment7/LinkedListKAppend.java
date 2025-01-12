c1711317dbeeeebf61176232ae9950b9df6a60f6
Ashish Kumar <ashishks1911@gmail.com>
2025-01-12 16:17:22 +0530

    Corrected and Simplified Linked list K append

--- src/org/codingblocks/assignment/assignment7/LinkedListKAppend.java
+++ src/org/codingblocks/assignment/assignment7/LinkedListKAppend.java
@@ -6,81 +6,61 @@ public class LinkedListKAppend {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        LinkNode list = new LinkNode();
+        Node head = null, tail = null;
         for (int i = 0; i < n; i++) {
-            list.insert(sc.nextInt());
+            int val = sc.nextInt();
+            Node nn = new Node(val);
+            if (head == null) {
+                head = nn;
+            } else {
+                tail.next = nn;
+            }
+            tail = nn;
         }
         int k = sc.nextInt();
+        Node newHead = head;
+        if (k > n) {
+            k = k % n;
+        }
+        if(k<n) {
+            Node curr = head;
+            for (int i = 0; i < size(head) - k - 1; i++) {
+                curr = curr.next;
+            }
+            newHead = curr.next;
+            curr.next = null;
 
-        if (k >= n) {
-            list.display();
-        } else {
-            int count = 1;
-            LinkNode.Node head = list.getHead();
-            LinkNode.Node temp = head;
-            while (temp != null) {
-                if (count == n - k)
-                    break;
-                count++;
+            Node temp = newHead;
+            while (temp.next != null) {
                 temp = temp.next;
             }
+            temp.next = head;
+        }
 
-            LinkNode.Node nn = temp.next;
-            LinkNode.Node tail = temp.next;
-            while (tail.next != null) {
-                tail = tail.next;
-            }
-            tail.next = head;
-            temp.next = null;
-            head = nn;
-            list.setHead(head);
-            list.display();
-
+        Node ans = newHead;
+        while (ans != null) {
+            System.out.print(ans.data + " ");
+            ans = ans.next;
         }
 
     }
 
-    static class LinkNode {
-        class Node {
-            int data;
-            Node next;
-        }
+    static class Node {
+        int data;
+        Node next;
 
-        public Node head;
-
-        public void insert(int item) {
-            Node nn = new Node();
-            nn.data = item;
-            if (head == null) {
-                head = nn;
-            } else {
-                Node temp = head;
-                while (temp.next != null) {
-                    temp = temp.next;
-                }
-                temp.next = nn;
-            }
+        public Node(int item) {
+            this.data = item;
         }
+    }
 
-        public void display() {
-            Node temp = head;
-            while (temp != null) {
-                System.out.print(temp.data + " ");
-                temp = temp.next;
-            }
-            System.out.println();
+    public static int size(Node head) {
+        Node curr = head;
+        int size = 0;
+        while (curr != null) {
+            size++;
+            curr = curr.next;
         }
-
-        public Node getNode() {
-            return new Node();
-        }
-
-        public Node getHead() {
-            return head;
-        }
-
-        public void setHead(Node node) {
-            head = node;
-        }
+        return size;
     }
 }
