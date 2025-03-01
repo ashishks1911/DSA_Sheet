7bd26ef6cc68f739bb2cb2441da5d6726e30a9a2
Ashish Kumar <ashishks1911@gmail.com>
2025-03-01 15:30:55 +0530

    Construct Binary tree from preorder and inorder

--- src/org/codingblocks/tree/binarytree/ConstructBinaryTreeFromPreorderAndInorder.java
+++ src/org/codingblocks/tree/binarytree/ConstructBinaryTreeFromPreorderAndInorder.java
@@ -1,2 +1,38 @@
-package org.codingblocks.tree.binarytree;public class ConstructBinaryTreeFromPreorderAndInorder {
+package org.codingblocks.tree.binarytree;
+
+public class ConstructBinaryTreeFromPreorderAndInorder {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return create(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+    }
+
+    public TreeNode create(int[] pre, int plo, int phi, int[] in, int ilo, int ihi) {
+        if (plo > phi || ilo > ihi) {
+            return null;
+        }
+        TreeNode node = new TreeNode(pre[plo]);
+        int idx = search(in, ilo, ihi, pre[plo]);
+        int ne = idx - ilo;                                 //no. of elements
+        node.left = create(pre, plo + 1, plo + ne, in, ilo, idx - 1);
+        node.right = create(pre, plo + ne + 1, phi, in, idx + 1, ihi);
+        return node;
+    }
+
+    public int search(int[] inorder, int si, int ei, int item) {
+        for (int i = si; i <= ei; i++) {
+            if (inorder[i] == item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
 }
