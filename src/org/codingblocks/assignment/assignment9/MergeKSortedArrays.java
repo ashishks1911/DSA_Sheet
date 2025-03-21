43ae439c78dd30f5fb83e51df40c7d815a3e3e99
Ashish Kumar <ashishks1911@gmail.com>
2025-03-21 17:53:30 +0530

    Assignment:9 added merge k sorted arrays

--- src/org/codingblocks/assignment/assignment9/MergeKSortedArrays.java
+++ src/org/codingblocks/assignment/assignment9/MergeKSortedArrays.java
@@ -1,2 +1,52 @@
-package org.codingblocks.assignment.assignment9;public class MergeKSortedArrays {
+package org.codingblocks.assignment.assignment9;
+
+import java.util.*;
+
+public class MergeKSortedArrays {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int arr[][] = new int[k][n];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        PriorityQueue<Element> pq = new PriorityQueue<>((a, b) -> a.value - b.value);
+
+        for (int i = 0; i < k; i++) {
+            pq.add(new Element(i, 0, arr[i][0]));
+        }
+
+        int[] ans = new int[k * n];
+        int t = 0;
+        while (!pq.isEmpty()) {
+            Element rv = pq.poll();
+            ans[t] = rv.value;
+            t++;
+            if (rv.index + 1 < arr[rv.array].length) {
+                pq.add(new Element(rv.array, rv.index + 1, arr[rv.array][rv.index + 1]));
+            }
+        }
+
+        for (int val : ans) {
+            System.out.print(val + " ");
+        }
+
+    }
+
+    static class Element {
+        int array;
+        int index;
+        int value;
+
+        public Element(int array, int index, int value) {
+            this.array = array;
+            this.index = index;
+            this.value = value;
+        }
+    }
+
 }
--- src/org/codingblocks/assignment/assignment9/MergeKSortedLists.java
+++ src/org/codingblocks/assignment/assignment9/MergeKSortedLists.java
@@ -1,2 +1,65 @@
-package org.codingblocks.assignment.assignment9;public class MergeKSortedLists {
+package org.codingblocks.assignment.assignment9;
+
+import java.util.*;
+
+public class MergeKSortedLists {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        Node[] lists = new Node[k];
+        for (int i = 0; i < k; i++) {
+            Node head = null;
+            Node tail = null;
+            for (int j = 0; j < n; j++) {
+                int val = sc.nextInt();
+                Node temp = new Node(val);
+                if (head == null) {
+                    head = temp;
+                    tail = temp;
+                } else {
+                    tail.next = temp;
+                    tail = tail.next;
+                }
+            }
+            lists[i] = head;
+
+        }
+
+        Node ans = mergeKLists(lists);
+        while (ans != null) {
+            System.out.print(ans.data + " ");
+            ans = ans.next;
+        }
+
+
+    }
+
+    public static Node mergeKLists(Node[] lists) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.data - b.data);
+        for (Node list : lists) {
+            pq.add(list);
+        }
+
+        Node dummy = new Node(0);
+        Node temp = dummy;
+        while (!pq.isEmpty()) {
+            Node rv = pq.poll();
+            dummy.next = rv;
+            dummy = dummy.next;
+            if (rv.next != null) {
+                pq.add(rv.next);
+            }
+        }
+        return temp.next;
+    }
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
 }
